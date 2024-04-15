5985a3e34358ea19bef9ded5ba270a6ce9e6c5b0
pengjian05 <pengjian05@baidu.com>
2024-04-15 22:08:27 +0800

    Add zoomed flag and event stopImmediatePropagation for Escape key in content.ts

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -5,6 +5,7 @@ export default defineContentScript({
   main() {
     const zoom_op = zoom(document)
 
+    let zoomed = false;
     let lastRightClickedElement: EventTarget | null = null;
     document.addEventListener('contextmenu', function (event) {
       lastRightClickedElement = event.target;
@@ -12,8 +13,22 @@ export default defineContentScript({
 
     document.addEventListener('keyup', function(event) {
       console.log("keypress key=", event.key)
-      if (event.key === 'Escape') {
+      if (zoomed && event.key === 'Escape') {
         zoom_op.out({})
+        zoomed = false;
+        event.stopImmediatePropagation();
+      }
+    })
+
+    document.addEventListener('keydown', function(event) {
+      if (zoomed && event.key === 'Escape') {
+        event.stopImmediatePropagation();
+      }
+    })
+
+    document.addEventListener('keypress', function(event) {
+      if (zoomed && event.key === 'Escape') {
+        event.stopImmediatePropagation();
       }
     })
 
@@ -25,6 +40,7 @@ export default defineContentScript({
             zoom_op.to({
               element: lastRightClickedElement,
             })
+            zoomed = true;
           } else {
             console.error("No valid element selected")
           }
