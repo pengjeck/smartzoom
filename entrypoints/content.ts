c2ee504dda1db39db16242848872401d1a997a21
pengjeck <pjwhusir@gmail.com>
2024-05-03 07:34:49 +0800

    Refactor background.ts and content.ts: improve zoom functionality and fix event handling.

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -5,47 +5,46 @@ export default defineContentScript({
   main() {
     const zoom_op = zoom(document)
 
-    let zoomed = false;
+    let zoomed = function () {
+      return Math.abs(zoom_op.zoomLevel() - 1) > 0.001
+    };
     let lastRightClickedElement: EventTarget | null = null;
     document.addEventListener('contextmenu', function (event) {
       lastRightClickedElement = event.target;
     })
 
-    document.addEventListener('keyup', function(event) {
-      console.log("keypress key=", event.key)
-      if (zoomed && event.key === 'Escape') {
+    document.addEventListener('mouseover', function (event) {
+      lastRightClickedElement = event.target;
+    })
+
+    document.addEventListener('keyup', function (event) {
+      console.log("keypress key=", event.key, "zoomed=", zoomed())
+      if (zoomed() && event.key === 'Escape') {
         zoom_op.out({})
-        zoomed = false;
         event.stopImmediatePropagation();
       }
-    })
 
-    document.addEventListener('keydown', function(event) {
-      if (zoomed && event.key === 'Escape') {
-        event.stopImmediatePropagation();
-      }
-    })
-
-    document.addEventListener('keypress', function(event) {
-      if (zoomed && event.key === 'Escape') {
-        event.stopImmediatePropagation();
-      }
-    })
-
-    browser.runtime.onMessage.addListener(
-      function (request, sender, sendResponse) {
-        console.log(request)
-        if (request.action === "zoomIn" && lastRightClickedElement) {
-          if (lastRightClickedElement && lastRightClickedElement instanceof HTMLElement) {
-            zoom_op.to({
-              element: lastRightClickedElement,
-            })
-            zoomed = true;
-          } else {
-            console.error("No valid element selected")
-          }
+      if (zoomed() == false && event.key === 'z') {
+        if (lastRightClickedElement && lastRightClickedElement instanceof HTMLElement) {
+          zoom_op.to({
+            element: lastRightClickedElement,
+          })
+        } else {
+          console.error("No valid element selected")
         }
       }
-    );
+    })
+
+    document.addEventListener('keydown', function (event) {
+      if (zoomed() && event.key === 'Escape') {
+        event.stopImmediatePropagation();
+      }
+    })
+
+    document.addEventListener('keypress', function (event) {
+      if (zoomed() && event.key === 'Escape') {
+        event.stopImmediatePropagation();
+      }
+    })
   },
 });
